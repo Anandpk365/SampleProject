d0c589dee12f943a17b8d8258e8e9fe3c6ed7931
Anand Kumar <anandpk365@gmail.com>
2022-10-15 11:23:26 +0530

    My first commit

---
+++ Practice1.java
@@ -0,0 +1,11 @@
+import java uitl.*;
+
+class Practce{
+
+          public static void main(String str[]){
+			  System.out.println("This is for Git command Practice");
+			  System.out.println("Yes this is for Git command Practice Only");
+		  }
+}
+
+	
